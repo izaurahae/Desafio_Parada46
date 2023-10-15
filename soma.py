6ff80c0ed8f3812ba6e0b63a4ba3c4ffc8dc9cca
Izaura Souza <106878780+izaurahae@users.noreply.github.com>
2023-10-15 12:48:55 -0300

    Add files via upload

---
+++ soma.py
@@ -0,0 +1,9 @@
+def soma(a, b):
+    resultado = a + b
+    return resultado
+
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+resultado = soma(numero1, numero2)
+print(f"A soma de {numero1} e {numero2} é igual a {resultado}") 
